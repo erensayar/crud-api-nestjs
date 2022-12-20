42a07fb6f6e4456bd02ca270ee2c96946aa356f9
erensayar <erensayar@yandex.com>
2022-12-20 19:11:29 +0300

    delete unnecessary things

--- src/hede/dto/update-hede.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateHedeDto } from './create-hede.dto';
-
-export class UpdateHedeDto extends PartialType(CreateHedeDto) {
-  id: number;
-}
--- src/hede/hede.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HedeService } from './hede.service';
-import { HedeController } from './hede.controller';
-
-@Module({
-  controllers: [HedeController],
-  providers: [HedeService]
-})
-export class HedeModule {}
