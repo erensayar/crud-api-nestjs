3d240a68b62d3ffa3f3682bae2d699157b33be34
erensayar <erensayar@yandex.com>
2022-12-20 19:12:55 +0300

    delete unnecessary things

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { NoteModule } from './note/note.module';
-import { HedeModule } from './hede/hede.module';
 
 @Module({
   imports: [
     UserModule,
-    NoteModule,
-    HedeModule
+    NoteModule
     //MongooseModule.forRoot('mongodb://localhost:27017/nest'),
   ]
 })
